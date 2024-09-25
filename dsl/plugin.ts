f6390fba9d9abfd37a3a6390dd3f9bdfc20fdf1e
NeroXie <xyh30902@163.com>
2024-09-25 22:18:51 +0800

    添加 DSL

---
+++ dsl/plugin.ts
@@ -0,0 +1,7 @@
+import * as d from 'rct-plugin-decorators/lib/decorators';
+import * as t from 'rct-plugin-decorators/lib/types';
+
+@d.Plugin('__RCTPlugin', '__version', t.PluginType.NULL, ['__owners'])
+export class RCTPlugin {
+
+}
